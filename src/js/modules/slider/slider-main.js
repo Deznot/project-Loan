b79c9dc1e26d23fb4855ac84bc3b06d2d2dd09b8
Denis <dean.angell@mail.ru>
2020-12-16 16:51:00 +0300

    add slider-prototype and mainslider

--- src/js/modules/slider/slider-main.js
+++ src/js/modules/slider/slider-main.js
@@ -1,9 +1,7 @@
-export default class Slider{
-    constructor(page, btns){
-        this.page = document.querySelector(page);
-        this.slides = this.page.children;
-        this.btns = document.querySelectorAll(btns);
-        this.slideIndex = 1;
+import Slider from './slider';
+export default class MainSlider extends Slider{
+    constructor(page,btns) {
+        super(page, btns);
     }
 
     showSlides(n){
@@ -41,5 +39,4 @@ export default class Slider{
 
         this.showSlides(this.slideIndex);
     }
-
 }
